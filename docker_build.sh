374d7fc277c3b641b7a829cdd0ac0fda7fba9ea0
racsoraul <racsoraul@gmail.com>
2019-03-05 12:42:47 -0600

    Double quote to avoid globbing and word splitting.

--- docker_build.sh
+++ docker_build.sh
@@ -4,4 +4,4 @@ if [ "$1" ]; then
     BUILD=$1
 fi
 echo "Building $BUILD..."
-docker build --build-arg USER_ID=$(id -u ${USER}) --build-arg GROUP_ID=$(id -g ${USER}) -f Dockerfile.$BUILD -t simbotic-cortex/$BUILD .
+docker build --build-arg USER_ID="$(id -u "${USER}")" --build-arg GROUP_ID="$(id -g "${USER}")" -f Dockerfile."$BUILD" -t simbotic-ros/"$BUILD" .
--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$1" == "gpu" ]; then
-    docker run --rm --runtime=nvidia -ti -v $(pwd):/sim --network=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_PTRACE simbotic-cortex/gpu
+    docker run --rm --runtime=nvidia -ti -v "$(pwd)":/sim --network=host -e DISPLAY="$DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_PTRACE simbotic-ros/gpu
 else
-    docker run --rm -ti -v $(pwd):/sim --network=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_PTRACE simbotic-cortex/cpu
+    docker run --rm -ti -v "$(pwd)":/sim --network=host -e DISPLAY="$DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_PTRACE simbotic-ros/cpu
 fi

b16056b35e27978dbdb0c3be93243a0dc13542d2
racsoraul <racsoraul@gmail.com>
2019-03-05 15:36:16 -0600

    Modified path to mount volume. bash_history added to gitignore. Updated bashrc with new colors.

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$1" == "gpu" ]; then
-    docker run --rm --runtime=nvidia -ti -v "$(pwd)":/sim --network=host -e DISPLAY="$DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_PTRACE simbotic-ros/gpu
+    docker run --rm --runtime=nvidia -ti -v "$(pwd)":/home/sim --network=host -e DISPLAY="$DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_PTRACE simbotic-ros/gpu
 else
-    docker run --rm -ti -v "$(pwd)":/sim --network=host -e DISPLAY="$DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_PTRACE simbotic-ros/cpu
+    docker run --rm -ti -v "$(pwd)":/home/sim --network=host -e DISPLAY="$DISPLAY" -v /tmp/.X11-unix:/tmp/.X11-unix --cap-add=SYS_PTRACE simbotic-ros/cpu
 fi

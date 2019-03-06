7a1c612cac14967ced180783c8ade3acc7314c88
racsoraul <racsoraul@gmail.com>
2019-03-06 13:13:49 -0600

    Run roscore on start.

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -8,5 +8,5 @@ source "$HOME"/.bashrc
 cd ~/simbotic_catkin_workspace
 catkin_make
 echo "source $HOME/simbotic_catkin_workspace/devel/setup.bash" > ~/.bashrc
-
+roscore > /dev/null 2>&1 &
 exec "$@"

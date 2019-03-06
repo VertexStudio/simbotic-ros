dab4aaa6ff4834b97251190e695b4699d8efde06
racsoraul <racsoraul@gmail.com>
2019-03-06 12:48:57 -0600

    FIX sourcing ROS packages on start.

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -2,14 +2,11 @@
 set -e
 
 # setup ROS environment
-
-echo "source /opt/ros/$ROS_DISTRO/setup.bash" > ~/.bashrc
 source "$HOME"/.bashrc
 
 # building ROS packages
-pushd ~/simbotic_catkin_workspace
+cd ~/simbotic_catkin_workspace
 catkin_make
-popd
-source "$HOME"/simbotic_catkin_workspace/devel/setup.bash
+echo "source $HOME/simbotic_catkin_workspace/devel/setup.bash" > ~/.bashrc
 
 exec "$@"

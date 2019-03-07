bd3266a9c6778d054378b334e605322b3ca9fcf6
racsoraul <racsoraul@gmail.com>
2019-03-06 18:39:35 -0600

    Base docker-compose.yml

--- docker_build.sh
+++ docker_build.sh
@@ -4,4 +4,7 @@ if [ "$1" ]; then
     BUILD=$1
 fi
 echo "Building $BUILD..."
-docker build --build-arg USER_ID="$(id -u "${USER}")" --build-arg GROUP_ID="$(id -g "${USER}")" -f Dockerfile."$BUILD" -t simbotic-ros/"$BUILD" .
+docker build --build-arg USER_ID="$(id -u "${USER}")" \
+    --build-arg GROUP_ID="$(id -g "${USER}")" \
+    -f Dockerfile."$BUILD" \
+    -t simbotic-ros/"$BUILD" .
